8280a0dc3cb9a41a1d658ee630f5d2cd81989a64
Khhhazov <SemenKhazov@gamil.com>
2022-05-05 22:15:54 +0300

    экран регистрации

--- app/src/main/java/com/example/passagge/activity/auth/LoginFragment.kt
+++ app/src/main/java/com/example/passagge/activity/auth/LoginFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.passagge.R
 
 class LoginFragment : Fragment() {
@@ -14,5 +16,13 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val secondButton = view.findViewById<Button>(R.id.fragment_login__second_button)
+        secondButton?.setOnClickListener {
+            findNavController().navigate(R.id.reg_fragment)
+        }
     }
 }
